4f55932093140385b7c5707d0c5a135691a850a7
VinsunB <vinsuncoding@gmail.com>
2016-05-31 00:09:42 -0400

    basic javascript on comments

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+$('.show_replies + .replies').hide();
+
+$('.show_replies').on('click', function(){
+
+$(this).next('.replies').slideToggle();
+
+});
+
+
+$('.reply_form_button + .reply_form').hide();
+
+$('.reply_form_button').on('click', function(){
+
+$(this).next('.reply_form').slideToggle();
+
+});
+
+
+
+});
